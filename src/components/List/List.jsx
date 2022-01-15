249ec0abdb16042844fcd42834f00e40f636c4c5
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-01-15 18:18:34 +0530

    completed maps section

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import useStyles from './styles.js';
+
 
 const List = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>List</h1> 
+        <div className={classes.container}>
+            <Typography variant = "h4">
+                Restaurants , Hotels & Attractions around You
+            </Typography>
+            <FormControl  className={classes.formControl}>
+                 <InputLabel>Type</InputLabel>
+                 <Select value={} onChange={}>
+                     <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                 </Select> 
+            </FormControl>
         </div>
     )
 }
